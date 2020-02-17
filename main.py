df4e740e64ad685b4cd9548f103b6b740566a5e8
bssughosh <bssughosh27@gmail.com>
2020-02-17 19:20:59 +0530

    added files

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from cv2 import imshow, imread, resize, waitKey, equalizeHist
+from cv2 import imshow, imread, resize, waitKey, equalizeHist, destroyAllWindows
 import os
 import numpy as np
 
 base_path = 'D:/Sem 6/IP/MiniProjectIP/images'
-file_name = 'Fabric10.jpg'
-file = os.path.join(base_path, file_name)
+file_names = []
+for i in range(1, 25):
+    file_names.append('Fabric' + str(i) + '.jpg')
 
+files = []
+for i in range(1, 25):
+    files.append(os.path.join(base_path, file_names[i - 1]))
+x = imread(files[0])
+imshow('', x)
+waitKey(0)
+destroyAllWindows()
