38e86ebe5ce9ff8af21f0c46686396fac1bcd872
bssughosh <bssughosh27@gmail.com>
2020-02-17 18:58:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from cv2 import imshow, imread, resize, waitKey, equalizeHist
+import os
+import numpy as np
+
+base_path = 'D:/Sem 6/IP/MiniProjectIP/images'
+file_name = 'Fabric10.jpg'
+file = os.path.join(base_path, file_name)
+
